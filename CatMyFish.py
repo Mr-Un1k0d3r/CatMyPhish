a025820985e2cd30ae66433f0f973d02a9a6f111
Mr-Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-03-07 10:35:34 -0500

    Update CatMyFish.py

--- CatMyFish.py
+++ CatMyFish.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 	exitone = True if "-exitone" in sys.argv else False
 
 	urls = {"expireddomain": {"get": "/domain-name-search/?q=", "post": "fdomainstart=&fdomain=&fdomainend=&flists%5B%5D=1&ftrmaxhost=0&ftrminhost=0&ftrbl=0&ftrdomainpop=0&ftrabirth_year=0&ftlds%5B%5D=2&button_submit=Apply+Filter&q=", "host": 
-"https://www.expireddomains.net", "referer": "https://www.expireddomains.net/domain-name-search/?q=test&searchinit=1"}, \
+"https://www.expireddomains.net", "referer": "https://www.expireddomains.net/domain-name-search/?q=&searchinit=1"}, \
 "bluecoat": {"get": "/rest/categorization", "post": "url=", "host": "https://sitereview.bluecoat.com", "referer": None}, \
 "checkdomain": {"get": "/cgi-bin/checkdomain.pl?domain=", "post": None, "host": "http://www.checkdomain.com"}}
 	# TODO: Need to add more to that list
